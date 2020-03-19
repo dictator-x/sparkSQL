56c1f5c854c09fb03d1c5999589f49958f61c604
dictator-x <dictator.xwu@gmail.com>
2020-03-19 13:10:28 -0400

    try dataframe basic api

--- src/main/scala/com/tt/sparksql/chapter04/DataFrameAPIApp.scala
+++ src/main/scala/com/tt/sparksql/chapter04/DataFrameAPIApp.scala
@@ -32,6 +32,30 @@ object DataFrameAPIApp {
 
     df.select($"name", ($"age"+10).as("new_age")).show()
 
+    val zips: DataFrame = spark.read.json("file:///home/dictator/work_space/sparkSQL/data/zips.json")
+
+    zips.printSchema()
+    zips.show()
+    zips.groupBy("city").count().show()
+    zips.show(5, false)
+
+    zips.head(3).foreach(println)
+
+    zips.take(5).foreach(println)
+
+    val count: Long = zips.count()
+    println(s"Total Counts: $count")
+
+    zips.select($"_id" as "id", $"city").filter($"pop" > 40000).show(10, false)
+    zips.filter($"pop" > 40000).withColumnRenamed("_id", "new_id").show(10, false)
+
+    import org.apache.spark.sql.functions._
+
+    zips.select("_id", "city", "pop", "state").filter(zips.col("state") === "CA").orderBy(desc("pop")).show(false)
+
+    zips.createOrReplaceTempView("zip")
+    spark.sql("select _id, city, pop, state from zip where state='CA' order by pop desc limit 10").show()
+
     spark.stop()
   }
 }
