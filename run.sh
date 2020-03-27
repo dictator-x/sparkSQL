6341a5f8288dc6e4450204c95ced0b25fd1af4c6
dictator-x <dictator.xwu@gmail.com>
2020-03-27 13:46:28 -0400

    dynamic support for the script

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ mvn clean package
 time=20181007
 PROCESS_DATE=`date --date='1 day ago' +%Y%m%d`
 ${SPARK_HOME}/bin/spark-submit \
---class com.tt.sparksql.chapter08.SparkApp \
---master local \
+--class $1 \
+--master yarn \
 --jars /home/dictator/.m2/repository/org/apache/kudu/kudu-client/1.7.0/kudu-client-1.7.0.jar,/home/dictator/.m2/repository/org/apache/kudu/kudu-spark2_2.11/1.7.0/kudu-spark2_2.11-1.7.0.jar \
 --conf spark.time=$time \
 --conf spark.raw.path="hdfs://centos:8020/access/$time/data-test.json" \
